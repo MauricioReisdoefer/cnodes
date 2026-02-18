401db44217f4e624d6a2f899e3e65e3bc8b62e14
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2026-02-18 16:55:57 -0300

    feat: creating vector2 new methods

--- src/math/vector2.c
+++ src/math/vector2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include "math/vector2.h"
 
 Vector2 Vector2_Zero()
@@ -43,14 +44,55 @@ Vector2 Vector2_Subtract(Vector2 a, Vector2 b)
     return result;
 }
 
-Vector2 Vector2_Normalize(Vector2 v);
-float Vector2_Dot(Vector2 a, Vector2 b);
+Vector2 Vector2_Normalize(Vector2 v)
+{
+    float length = sqrtf(v.x * v.x + v.y * v.y);
 
-float Vector2_Length(Vector2 v);
-float Vector2_LengthSquared(Vector2 v);
+    if (length == 0.0f)
+        return (Vector2){0.0f, 0.0f};
 
-float Vector2_Distance(Vector2 a, Vector2 b);
-float Vector2_DistanceSquared(Vector2 a, Vector2 b);
+    return (Vector2){v.x / length, v.y / length};
+}
 
-Vector2 Vector2_Lerp(Vector2 a, Vector2 b, float t);
-Vector2 Vector2_Scale(Vector2 a, float b);
+float Vector2_Dot(Vector2 a, Vector2 b)
+{
+    return a.x * b.x + a.y * b.y;
+}
+
+float Vector2_Length(Vector2 v)
+{
+    return sqrtf(v.x * v.x + v.y * v.y);
+}
+
+float Vector2_LengthSquared(Vector2 v)
+{
+    return v.x * v.x + v.y * v.y;
+}
+
+float Vector2_Distance(Vector2 a, Vector2 b)
+{
+    float dx = b.x - a.x;
+    float dy = b.y - a.y;
+    return sqrtf(dx * dx + dy * dy);
+}
+
+float Vector2_DistanceSquared(Vector2 a, Vector2 b)
+{
+    float dx = b.x - a.x;
+    float dy = b.y - a.y;
+    return dx * dx + dy * dy;
+}
+
+Vector2 Vector2_Lerp(Vector2 a, Vector2 b, float t)
+{
+    return (Vector2){
+        a.x + (b.x - a.x) * t,
+        a.y + (b.y - a.y) * t};
+}
+
+Vector2 Vector2_Scale(Vector2 a, float b)
+{
+    return (Vector2){
+        a.x * b,
+        a.y * b};
+}
